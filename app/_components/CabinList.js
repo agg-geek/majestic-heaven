d437cf44af41e1f5995d4c891c5db6556ffce071
agg-geek <92445847+agg-geek@users.noreply.github.com>
2024-09-15 20:18:06 +0530

    Implement filtering cabins

--- app/_components/CabinList.js
+++ app/_components/CabinList.js
@@ -1,14 +1,28 @@
 import CabinCard from '@/app/_components/CabinCard';
 import { getCabins } from '@/app/_lib/data-service';
 
-export default async function CabinList() {
+export default async function CabinList({ filter }) {
 	const cabins = await getCabins();
 
 	if (!cabins.length) return null;
 
+	const displayedCabins = cabins.filter(cabin => {
+		const { maxCapacity } = cabin;
+		if (
+			filter === 'all' ||
+			(filter === 'small' && maxCapacity >= 1 && maxCapacity <= 3) ||
+			(filter === 'medium' && maxCapacity >= 4 && maxCapacity <= 7) ||
+			(filter === 'large' && maxCapacity >= 8 && maxCapacity <= 10)
+		) {
+			return true;
+		}
+
+		return false;
+	});
+
 	return (
 		<div className="grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14">
-			{cabins.map(cabin => (
+			{displayedCabins.map(cabin => (
 				<CabinCard cabin={cabin} key={cabin.id} />
 			))}
 		</div>
--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,14 +1,23 @@
 import { Suspense } from 'react';
 import CabinList from '@/app/_components/CabinList';
 import Spinner from '@/app/_components/Spinner';
+import Filter from '@/app/_components/Filter';
 
-export const revalidate = 3600;
+// revalidate will not have any effect now because we use searchParams
+// so the route is dynamically generated now
+// export const revalidate = 3600;
 
 export const metadata = {
 	title: 'All cabins',
 };
 
-export default function CabinsPage() {
+export default function CabinsPage({ searchParams }) {
+	// passing state from client to server
+	// searchParams will only be available on the page and not inside components
+
+	// cabins capacity can be: small, medium, large, all
+	const filter = searchParams?.capacity ?? 'all';
+
 	return (
 		<div>
 			<h1 className="text-4xl mb-5 text-accent-400 font-medium">
@@ -23,8 +32,12 @@ export default function CabinsPage() {
 				for a peaceful, calm vacation. Welcome to paradise.
 			</p>
 
+			<div className="flex justify-end mb-8">
+				<Filter />
+			</div>
+
 			<Suspense fallback={<Spinner />}>
-				<CabinList />
+				<CabinList filter={filter} />
 			</Suspense>
 		</div>
 	);
