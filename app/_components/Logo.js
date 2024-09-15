9379bb3f5e44a4943c6c59c08930e6f100ada197
agg-geek <92445847+agg-geek@users.noreply.github.com>
2024-09-15 10:06:34 +0530

    Optimise images

--- app/_components/Logo.js
+++ app/_components/Logo.js
@@ -1,14 +1,32 @@
+import Image from 'next/image';
 import Link from 'next/link';
+
+// import image so you can choose to leave the width and height properties
+// this way, additional properties like quality can be specified
+// as next.js already knows beforehand about the image
+import { logo } from '@/public/logo.png';
 
 export default function Logo() {
 	return (
 		<Link href="/" className="flex items-center gap-4 z-10">
-			<img
+			{/* width and height properties reqd */}
+			<Image
 				src="/logo.png"
-				height="60"
 				width="60"
+				height="60"
 				alt="The Majestic Heaven logo"
 			/>
+
+			{/* this way, additional properties like quality can be specified
+				the width and height properties can be omitted in this method */}
+			<Image
+				src={logo}
+				// quality={10}
+				width="60"
+				height="60"
+				alt="The Majestic Heaven logo"
+			/>
+
 			<span className="text-xl font-semibold text-primary-100">
 				The Majestic Heaven
 			</span>
