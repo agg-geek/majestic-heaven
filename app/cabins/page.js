9e3630117a03813a5d1f6f2f0eb51944c8fb6c86
agg-geek <92445847+agg-geek@users.noreply.github.com>
2024-09-15 15:33:30 +0530

    Suspend CabinList

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,13 +1,12 @@
-import CabinCard from '@/app/_components/CabinCard';
-import { getCabins } from '@/app/_lib/data-service';
+import { Suspense } from 'react';
+import CabinList from '@/app/_components/CabinList';
+import Spinner from '@/app/_components/Spinner';
 
 export const metadata = {
 	title: 'All cabins',
 };
 
-export default async function CabinsPage() {
-	const cabins = await getCabins();
-
+export default function CabinsPage() {
 	return (
 		<div>
 			<h1 className="text-4xl mb-5 text-accent-400 font-medium">
@@ -22,13 +21,14 @@ export default async function CabinsPage() {
 				for a peaceful, calm vacation. Welcome to paradise.
 			</p>
 
-			{cabins.length > 0 && (
-				<div className="grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14">
-					{cabins.map(cabin => (
-						<CabinCard cabin={cabin} key={cabin.id} />
-					))}
-				</div>
-			)}
+			{/* the loading.js in cabins will block the complete cabins page
+				since the above cabins page desc can be shown without a loader
+				use a supsense for the cabinlist
+				this suspense will override the global loading.js with Spinner
+				for the cabinlist component */}
+			<Suspense fallback={<Spinner />}>
+				<CabinList />
+			</Suspense>
 		</div>
 	);
 }
