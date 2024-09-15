0adbb4ed358d1f716385a7740cd8548d11cff0e0
agg-geek <92445847+agg-geek@users.noreply.github.com>
2024-09-15 10:16:27 +0530

    Add homepage image

--- app/_components/Logo.js
+++ app/_components/Logo.js
@@ -1,27 +1,11 @@
 import Image from 'next/image';
 import Link from 'next/link';
 
-// import image so you can choose to leave the width and height properties
-// this way, additional properties like quality can be specified
-// as next.js already knows beforehand about the image
-import logo from '@/public/logo.png';
-
 export default function Logo() {
 	return (
 		<Link href="/" className="flex items-center gap-4 z-10">
-			{/* width and height properties reqd */}
 			<Image
 				src="/logo.png"
-				width="60"
-				height="60"
-				alt="The Majestic Heaven logo"
-			/>
-
-			{/* this way, additional properties like quality can be specified
-				the width and height properties can be omitted in this method */}
-			<Image
-				src={logo}
-				// quality={10}
 				width="60"
 				height="60"
 				alt="The Majestic Heaven logo"
--- app/page.js
+++ app/page.js
@@ -1,10 +1,29 @@
 import Link from 'next/link';
-import Logo from '@/app/_components/Logo';
+import Image from 'next/image';
+import bg from '@/public/bg.png';
 
 export default function AppPage() {
 	return (
-		<div>
-			<h1>Home page</h1>
+		<div className="mt-24">
+			<Image
+				src={bg}
+				fill
+				className="object-cover object-top"
+				placeholder="blur"
+				alt="Mountains and forests with two cabins"
+			/>
+
+			<div className="relative z-10 text-center">
+				<h1 className="text-8xl text-primary-50 mb-10 tracking-tight font-normal">
+					Welcome to paradise.
+				</h1>
+				<Link
+					href="/cabins"
+					className="bg-accent-500 px-8 py-6 text-primary-800 text-lg font-semibold hover:bg-accent-600 transition-all"
+				>
+					Explore luxury cabins
+				</Link>
+			</div>
 		</div>
 	);
 }
